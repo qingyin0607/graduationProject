2d456d918e45b5a56e83327e3fc910b9f08f4bad
xiaoyiquan <919696519@qq.com>
2021-01-03 15:19:05 +0800

    1.指定扫包路径

--- schoolmapsystem/src/main/java/com/graduationproject/schoolmapsystem/SchoolmapsystemApplication.java
+++ schoolmapsystem/src/main/java/com/graduationproject/schoolmapsystem/SchoolmapsystemApplication.java
@@ -1,9 +1,11 @@
 package com.graduationproject.schoolmapsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.graduationproject.schoolmapsystem.mapper")
 public class SchoolmapsystemApplication {
 
     public static void main(String[] args) {
